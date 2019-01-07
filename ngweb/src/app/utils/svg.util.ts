fac0b7b563e18967f3664bd6a4dbe603dd6e97bd
liu123 <liu___k@outlook.com>
2019-01-07 18:10:41 +0800

    图标简单修改

--- ngweb/src/app/utils/svg.util.ts
+++ ngweb/src/app/utils/svg.util.ts
@@ -3,17 +3,19 @@ import { DomSanitizer } from '@angular/platform-browser';
 
 export const loadSvgResources=(ir:MatIconRegistry, ds:DomSanitizer)=>{
     const imgDir='assets/img';
-    const sidebarDir=`${imgDir}/sidebar`;
+    const dateDir=`${imgDir}/sidebar`;
     const dayDir = `${imgDir}/days`;
     const avatarDir=`${imgDir}/avatar`;
     const iconDir=`${imgDir}/icons`;
-    ir.addSvgIconSetInNamespace('unassigned',ds.bypassSecurityTrustResourceUrl(`${avatarDir}/unassigned.svg`));
+    //集合
+    ir.addSvgIcon('unassigned',ds.bypassSecurityTrustResourceUrl(`${avatarDir}/unassigned.svg`));
     ir.addSvgIconSetInNamespace('avatars',ds.bypassSecurityTrustResourceUrl(`${avatarDir}/avatars.svg`));
-    ir.addSvgIcon('day',ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/day.svg`));
-    ir.addSvgIcon('month',ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/month.svg`));
-    ir.addSvgIcon('project',ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/project.svg`));
-    ir.addSvgIcon('projects',ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/projects.svg`));
-    ir.addSvgIcon('week',ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/week.svg`));
+
+    ir.addSvgIcon('day',ds.bypassSecurityTrustResourceUrl(`${dateDir}/day.svg`));
+    ir.addSvgIcon('month',ds.bypassSecurityTrustResourceUrl(`${dateDir}/month.svg`));
+    ir.addSvgIcon('project',ds.bypassSecurityTrustResourceUrl(`${dateDir}/project.svg`));
+    ir.addSvgIcon('projects',ds.bypassSecurityTrustResourceUrl(`${dateDir}/projects.svg`));
+    ir.addSvgIcon('week',ds.bypassSecurityTrustResourceUrl(`${dateDir}/week.svg`));
     ir.addSvgIcon('move',ds.bypassSecurityTrustResourceUrl(`${iconDir}/move.svg`));
     ir.addSvgIcon('add',ds.bypassSecurityTrustResourceUrl(`${iconDir}/add.svg`));
     ir.addSvgIcon('delete',ds.bypassSecurityTrustResourceUrl(`${iconDir}/delete.svg`));
