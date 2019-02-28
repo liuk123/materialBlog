789d1499aae228e22d8a0c8c51f62ad30712e234
liu123 <liu___k@outlook.com>
2019-02-28 20:35:55 +0800

    路由修改 有bug

--- ngweb/src/app/navigation/navigation-home/navigation-home.component.ts
+++ ngweb/src/app/navigation/navigation-home/navigation-home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-home',
   templateUrl: './navigation-home.component.html',
   styleUrls: ['./navigation-home.component.scss']
 })
-export class NavigationHomeComponent implements OnInit {
+export class NavigationHomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    
+  }
+  
+  ngOnDestroy(){
+    console.log(123)
+    this.router.navigate([{ outlets: { popup: null }}]);
   }
 
 }
